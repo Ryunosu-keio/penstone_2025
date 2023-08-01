ec938b52f60c6ac8a748e147051a1c26c6940487
rikuriku1999 <kalashnikova1120@gmail.com>
2023-08-01 15:21:19 +0900

    全画面

--- show_images_back.py
+++ show_images_back.py
@@ -1,11 +1,9 @@
-
 import os
 import time
 from PIL import Image
 import matplotlib.pyplot as plt
 import asyncio
 import websockets
-
 
 def display_images(folder_path, delay):
     # フォルダ内のファイル名を取得し、アルファベット順にソート
@@ -14,25 +12,30 @@ def display_images(folder_path, delay):
     # 画像表示のためのfigureとaxesを生成
     fig, ax = plt.subplots()
 
+    # ウィンドウを全画面表示に設定
+    plt.get_current_fig_manager().window.state('zoomed')
+    
+
+    # サブプロットの余白をすべて0に設定
+    plt.subplots_adjust(left=-0.5, bottom=0, top=1, right=1)
+
     start_time = time.time()  # 初期時間を記録
     for image_file in image_files:
+        print(image_file)
         # ファイルが画像であることを確認
         if image_file.lower().endswith(('.png', '.jpg', '.jpeg')):
             image_path = os.path.join(folder_path, image_file)
-            img = Image.open(image_path)
-            
+            img = Image.open(image_path)           
             # ここを変えると画像のサイズを変更できる
-            img = img.resize((int(img.width * 0.6), int(img.height * 0.05)))
+            img = img.resize((int(img.width * 0.6), int(img.height * 0.9)))
             # 画像表示位置を変更
-            ax.set_position([0.4, -0.2, 1, 1])
-
-            # 画像を表示
+            # ax.set_position([0, 0, 1, 1])
             ax.imshow(img)
             plt.axis('off')  # 軸の非表示
 
             # 画像のタイトルをファイル名にする
             plt.title("")
-            
+
             # 画像を表示
             plt.draw()
             plt.pause(0.01)
@@ -52,16 +55,16 @@ def display_images(folder_path, delay):
     plt.close()
 
 # 使用例
-# display_images('experiment_images/', 2.5)
+display_images('experiment_images/', 2.5)
 
-async def client():
-    # Connect to the server
-    async with websockets.connect("ws://192.168.6.2:8765") as websocket:
-        # Send "start" message
-        await websocket.send("start")
+# async def client():
+#     # Connect to the server
+#     async with websockets.connect("ws://192.168.6.2:8765") as websocket:
+#         # Send "start" message
+#         await websocket.send("start")
 
-        # Start displaying random chars
-        display_images('experiment_images/', 2.5)
+#         # Start displaying random chars
+#         display_images('experiment_images/', 2.5)
 
-# Start the client
-asyncio.get_event_loop().run_until_complete(client())
+# # Start the client
+# asyncio.get_event_loop().run_until_complete(client())
