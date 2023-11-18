fb3bf426c19ad0fa2751c1746531dc892c930a18
ryunosukeio <128016362+ryunosukeio@users.noreply.github.com>
2023-11-18 21:49:33 +0900

    contrast_sensitivity

--- analysis/hist_dio_cor.py
+++ analysis/hist_dio_cor.py
@@ -3,25 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import combinations
 
-df = pd.read_excel("../data/final_part2/add_contrast.xlsx")
-df = df[df["hist_bright"] < 40]
+df = pd.read_excel("../data/final_part1/add_contrast.xlsx")
+# df = df[df["hist_bright"] < 40]
 
-# 判定する基準のカラム
-criteria_columns = ['gamma', 'brightness', 'contrast', 'sharpness', 'equalization']
+# # 判定する基準のカラム
+# criteria_columns = ['gamma', 'brightness', 'contrast', 'sharpness', 'equalization']
 
-# 5つのパラメータから3つを選ぶすべての組み合わせを生成
-combinations_of_criteria = list(combinations(criteria_columns, 3))
+# # 5つのパラメータから3つを選ぶすべての組み合わせを生成
+# combinations_of_criteria = list(combinations(criteria_columns, 3))
 
-# 各組み合わせに対してフィルタリングを行う
-for combination in combinations_of_criteria:
-    print(combination)
-    # 組み合わせの中で、3つの基準すべてが0でない行をフィルタリング
-    filtered_df = df[(df[list(combination)] != 0).all(axis=1)]
+# # 各組み合わせに対してフィルタリングを行う
+# for combination in combinations_of_criteria:
+#     print(combination)
+#     # 組み合わせの中で、3つの基準すべてが0でない行をフィルタリング
+#     filtered_df = df[(df[list(combination)] != 0).all(axis=1)]
 
-    # 散布図のプロット
-    plt.scatter(filtered_df["brightness_ratio"], filtered_df["diopter"])
-    plt.xlabel("brightness_ratio")
-    plt.ylabel("diopter")
-    plt.title(f"Combination: {combination}")
-    plt.show()
+#     # 散布図のプロット
+#     plt.scatter(filtered_df["brightness_ratio"], filtered_df["diopter"])
+#     plt.xlabel("brightness_ratio")
+#     plt.ylabel("diopter")
+#     plt.title(f"Combination: {combination}")
+#     plt.show()
+
+
+# 散布図のプロット
+plt.scatter(df["brightness_ratio"], df["diopter"])
+plt.xlabel("brightness_ratio")
+plt.ylabel("diopter")
+plt.show()
 
--- analysis/masking.py
+++ analysis/masking.py
@@ -47,30 +47,47 @@ def calculate_contrast(threshold_image, gray_image):
     return digit_brightness, non_digit_brightness, brightness_ratio
 
 
-def contrast_to_df(path):
-    df = pd.read_excel(path)
+def contrast_to_df(df):
     for index, row in tqdm(df.iterrows()):
         # image_path = row["image_path"]
-        image_name = row["image_name"]
-        # print(image_path)
-        image_path = glob.glob(
-            "../experiment_images/*/" + image_name + ".jpg")[0]
-        image_figure = row["figure"]
-        # print(image_figure)
-        image = cv2.imread(image_path)
-        # 画像をグレースケールに変換
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # show_image(gray_image)
-        threshold_image = maskedByFigure(image_figure)
-        digit_brightness, non_digit_brightness, brightness_ratio = calculate_contrast(
-            threshold_image, gray_image)
-        df.loc[index, "digit_brightness"] = digit_brightness
-        df.loc[index, "non_digit_brightness"] = non_digit_brightness
-        df.loc[index, "brightness_ratio"] = brightness_ratio
+        try:
+            image_name = row["image_name"]
+            # print(image_path)
+            image_path = glob.glob("../experiment_images/*/" + image_name)[0]
+            print(image_path)
+            image_figure = row["figure"]
+            # print(image_figure)
+            image = cv2.imread(image_path)
+            # 画像をグレースケールに変換
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # show_image(gray_image)
+            threshold_image = maskedByFigure(image_figure)
+            digit_brightness, non_digit_brightness, brightness_ratio = calculate_contrast(
+                threshold_image, gray_image)
+            df.loc[index, "digit_brightness"] = digit_brightness
+            df.loc[index, "non_digit_brightness"] = non_digit_brightness
+            df.loc[index, "brightness_ratio"] = brightness_ratio
+        except IndexError :
+            pass
+    df = df.dropna()
     return df
 
 
 if __name__ == "__main__":
-    path = "../data/updated_dataframe.xlsx"
-    df = contrast_to_df(path)
-    df.to_excel("../data/final_part2/add_contrast.xlsx", index=False)
+    path = "../data/final_part1/final_bright_add_modified.xlsx"
+    df = pd.read_excel(path)
+    #明るいとき
+    df = df[df["figure"] != "Q"]
+    df = df[df["figure"] != "J"]
+    df = df[df["image_name"] != "33_7_brightness19.0_sharpness0.85_gamma0.99.jpg"]
+    df = df[df["image_name"] != "36_9_sharpness0.28_equalization24.0.jpg"]
+    df = df[df["image_name"] != "20_I_gamma0.65_sharpness0.12_equalization9.4.jpg"]
+    df = df[df["image_name"] != "32_2_contrast1.1_sharpness0.65_equalization5.8.jpg"]
+    df = df[df["image_name"] != "10_S_brightness16.0_gamma0.8.jpg"]
+    df = df[df["image_name"] != "1_E_sharpness0.28_contrast0.84_equalization16.0.jpg"]
+    df = df[df["image_name"] != "18_2_gamma0.87_sharpness0.79_equalization29.0.jpg"]
+
+
+
+    df = contrast_to_df(df)
+    df.to_excel("../data/final_part1/add_contrast.xlsx", index=False)
