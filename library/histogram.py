bd3c1e775829ac73a0ceda2a65d0fe038925f91d
ryunosukeio <128016362+ryunosukeio@users.noreply.github.com>
2023-12-07 16:20:34 +0900

    hist_analyze

--- library/histogram.py
+++ library/histogram.py
@@ -5,9 +5,9 @@ import numpy as np
 def color_hist(filename):
     img = np.asarray(Image.open(filename).convert("RGB")).reshape(-1,3)
     plt.hist(img, color=["red", "green", "blue"], bins=128)
-    # plt.show()
+    plt.show()
     plt.xlim(0, 255)
-    plt.savefig('histogram/original_hist.png')
+    # plt.savefig('histogram/original_hist.png')
 
 
 color_hist("photos/3.jpg")
